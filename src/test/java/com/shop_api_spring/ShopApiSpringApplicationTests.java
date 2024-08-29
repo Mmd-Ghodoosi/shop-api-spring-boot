341e2697b7d97d01dc4d610b042fa686403cda97
mmd-ghodoosi <mamadakgh@gmail.com>
2024-08-29 15:03:58 +0330

    configuring shop-api-spring

---
+++ src/test/java/com/shop_api_spring/ShopApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop_api_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
