196767a43d838d04ca479b76f6795618256e2994
mmd-ghodoosi <mamadakgh@gmail.com>
2024-09-05 20:28:48 +0330

    added products dto

---
+++ src/main/java/com/shop_api_spring/products/dto/productDto.java
@@ -0,0 +1,17 @@
+package com.shop_api_spring.products.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+public class productDto {
+        @NotEmpty(message = "name can not be empty")
+        private String name;
+        @NotEmpty(message = "price can not be empty")
+        private String price;
+        @NotEmpty(message = "colors can not be empty")
+        private String colors;
+        @NotEmpty(message = "description can not be empty")
+        private String description;
+
+}
